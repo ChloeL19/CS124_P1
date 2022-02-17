4be9ca75afe877e92b45f14b8b737d6125cec51c
Chloe Loughridge <cloughridge@college.harvard.edu>
2022-02-17 15:15:07 -0500

    beginning the skeleton

---
+++ randmst.cpp
@@ -0,0 +1,22 @@
+#include <cstring>
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct graph {
+    // do graphy stuff
+};
+
+/*
+Generate a complete graph of n vertices, where the weight
+of each edge is a real number chosen uniformly at random on
+[0,1].
+*/
+graph* gen1D (int n) {
+
+}
+
+int main () {
+
+}
