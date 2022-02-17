2abd2aa7275b111d23b9d1917bcd0612248ba006
Chloe Loughridge <cloughridge@college.harvard.edu>
2022-02-17 15:31:35 -0500

    beginning the skeleton 2.0

--- randmst.cpp
+++ randmst.cpp
@@ -2,10 +2,18 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <set>
 using namespace std;
 
+struct vertex {
+    int dim; // the dimension of the vertex
+    vector<int> vert; // the vertex values
+};
+
+// a graph is a set of vertices
 struct graph {
-    // do graphy stuff
+    set<vertex> vertices;
 };
 
 /*
@@ -16,6 +24,23 @@ of each edge is a real number chosen uniformly at random on
 graph* gen1D (int n) {
 
 }
+
+/* 
+Helper function for computing euclidean distance.
+*/
+float euclideanDist (vertex* v1, vertex* v2) {
+
+}
+
+/*
+Generate graph on n vertices, where vertices are points chosen
+uniformly at random from inside the unit square.
+*/
+graph* gen2D (){
+
+}
+
+// TODO: fill in other functions here based on pset description
 
 int main () {
 
