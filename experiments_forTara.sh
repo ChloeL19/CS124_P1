1a2f42667ae1bf72f0452d212876cc5cbfee9a42
TaraNadella <70873323+TaraNadella@users.noreply.github.com>
2022-02-25 11:09:46 -0500

    Update experiments_forTara.sh

--- experiments_forTara.sh
+++ experiments_forTara.sh
@@ -2,9 +2,9 @@
 
 # 1D graphs
 echo "---------------beginning 0-dim experiment, n in increasing order-------------"
-./randmst_noheap 0 262144 5 0
+./randmst_noheap 0 262144 1 0
 echo "-----------------------------------------------------------------------------"
 # #2D graphs
 echo "---------------beginning 2-dim experiment, n in increasing order-------------"
-./randmst_noheap 0 262144 5 2
+./randmst_noheap 0 262144 1 2
 echo "-----------------------------------------------------------------------------"
